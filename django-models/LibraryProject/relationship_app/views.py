a8378fe96e14998b5f188f0ab3c2e5d213554569
mr-robot-del <fenzyaboy4life@gmail.com>
2025-09-19 15:10:14 -0700

    Utilize Django’s built-in views fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,8 @@ from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def list_books(request):
